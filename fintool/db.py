533100d12e305e820814ffe38ad8716e2cb3bafc
Isaias Ramirez <soote1.ir@gmail.com>
2022-01-21 00:21:43 -0600

    Implement CsvDb
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- fintool/db.py
+++ fintool/db.py
@@ -5,9 +5,13 @@ that can operate on data using different storage
 mechanisms.
 
 Supported clients:
-    - FileDb
+    - CsvDb
 
 """
+
+
+import csv
+import pathlib
 
 
 class UnsupportedDbTypeError(Exception):
@@ -44,41 +48,138 @@ class AbstractDb:
     """Abstract class to define db behavior.
     """
 
-    def add(self, item):
-        pass
-
-    def remove(self, item):
-        pass
-
-    def get(self, item):
-        pass
-
-    def edit(self, old, new):
-        pass
-
-
-
-class FileDb(AbstractDb):
-    """A db object that operates on files.
-    """
-
-    def __init__(self, cfg):
-        self._cfg = cfg
-
     def add_record(self, record):
         pass
 
-    def remove_record(self, record_id):
+    def remove_record(self, record):
         pass
 
-    def get_record(self, record_id):
+    def get_record(self, filter):
         pass
 
-    def get_records(self, tags):
+    def get_records(self, filters):
         pass
 
-    def edit_record(self, record_id, new_record):
+    def edit_record(self, old, new):
         pass
 
 
-SUPPORTED_TYPES = {'file': FileDb}
+class CsvDb(AbstractDb):
+    """A db object that operates on csv files.
+    """
+
+    HOMEDIR = '~/.fintool'
+    RECORDS_FILE = 'records.csv'
+    RECORDS_FILE_TMP = 'records.csv.tmp'
+
+    def __init__(self, homedir=HOMEDIR):
+        # create home dir
+        self._homedir_path = pathlib.Path(homedir).expanduser()
+        self._homedir_path.mkdir(parents=True, exist_ok=True)
+
+        # create path to records file
+        self._records_file = self._homedir_path.joinpath(self.RECORDS_FILE)
+        self._records_file_tmp = self._homedir_path.joinpath(
+            self.RECORDS_FILE_TMP
+        )
+
+    def add_record(self, record):
+        """Add a new record into csv file.
+
+        Args:
+            record (dict): A dictionary representing the new record.
+        """
+        file_exists = self._records_file.is_file()
+        with self._records_file.open('a', newline='') as csvfile:
+            field_names = record.keys()
+            writer = csv.DictWriter(csvfile, fieldnames=field_names)
+
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(record)
+
+    def remove_record(self, id_field, id_value):
+        """Remove a record from csv file.
+
+        Args:
+            id_field (str): A string representing record's id field
+            id_value (str): A string representing record's id value
+        """
+        with self._records_file.open('r', newline='') as records_csv_file:
+            # create reader and get field names
+            reader = csv.DictReader(records_csv_file)
+            # field_names can't be None or DictWriter will complain
+            field_names = reader.fieldnames if reader.fieldnames else []
+
+            with self._records_file_tmp.open('w', newline='') as tmp_csv_file:
+                # create writer to dump data in tmp file
+                writer = csv.DictWriter(tmp_csv_file, fieldnames=field_names)
+                writer.writeheader()
+
+                # resume reading and write each row into tmp file
+                for row in reader:
+                    if row[id_field] != id_value:
+                        writer.writerow(row)
+
+        # replace original csv with tmp file
+        pathlib.Path(self._records_file_tmp).replace(self._records_file)
+
+    def get_records(self, filters=None):
+        """Return records from csv file that matches any filter in filters
+
+        Args:
+            filters (dict): a dictionary mapping keys with target values.
+        """
+        with self._records_file.open('r', newline='') as csvfile:
+            result = []
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                # filter records if filters is not None
+                if filters:
+                    for key, values in filters.items():
+                        if key in row:
+                            for value in values:
+                                # add row if matches any value and stop loop
+                                if row[key] == value:
+                                    result.append(row)
+                                    break
+                # otherwise add all records
+                else:
+                    result.append(row)
+
+            return result
+
+    def edit_record(self, id_field, id_value, new_record):
+        """Update a record with a new set of values while keeping id.
+
+        Args:
+            id_field (str): A string representing the name of id field
+            id_value (str): A string representing the value of id field
+            new_record (dict): A dictionary representing the new set of values
+        """
+        with self._records_file.open('r', newline='') as records_csv_file:
+            # create reader and get field names
+            reader = csv.DictReader(records_csv_file)
+            # field_names can't be None or DictWriter will complain
+            field_names = reader.fieldnames if reader.fieldnames else []
+
+            with self._records_file_tmp.open('w', newline='') as tmp_csv_file:
+                # create writer to dump data in tmp file
+                writer = csv.DictWriter(tmp_csv_file, fieldnames=field_names)
+                writer.writeheader()
+
+                # start reading rows from original csv file
+                for row in reader:
+                    # write row to tmp file if it is not the target row
+                    if row[id_field] != id_value:
+                        writer.writerow(row)
+                    # otherwise write new record
+                    else:
+                        writer.writerow(new_record)
+
+        # replace original csv with tmp file
+        pathlib.Path(self._records_file_tmp).replace(self._records_file)
+
+
+SUPPORTED_TYPES = {'csv': CsvDb}
