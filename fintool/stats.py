e040e235ccf056acd8cba75178a87d547436c215
Isaias Ramirez <soote1.ir@gmail.com>
2022-02-12 19:39:39 -0600

    Add prototypes for stats module
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

---
+++ fintool/stats.py
@@ -0,0 +1,19 @@
+"""
+This module provides helper classes to perform statistic analysis
+on data sets.
+"""
+
+
+class StatsHelper:
+    """An utility class to calculate stats on a dataset.
+    """
+    @classmethod
+    def create_stats(cls, stats_type, data_set):
+        """Identify the type of stats and calculate
+        them by calling the appropriate method.
+        """
+
+    @classmethod
+    def overall_summary(cls, data_set):
+        """Calculate an overall summary from data_set.
+        """
