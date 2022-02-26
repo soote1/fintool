8494ab04c794d5e7d6e19c55399a61852ac74d58
Isaias Ramirez <soote1.ir@gmail.com>
2022-02-26 14:30:16 -0600

    Implement stats helper
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- fintool/stats.py
+++ fintool/stats.py
@@ -4,23 +4,37 @@ on data sets.
 """
 import datetime
 
+from fintool.logging import LoggingHelper
+
 
 class StatsHelper:
     """An utility class to calculate stats on a dataset.
     """
     TRANSACTIONS = 'transactions'
     TOTAL_PER_TAG = 'total_per_tag'
+    OVERALL_SUMMARY = 'overall_summary'
 
-    @classmethod
-    def create_stats(cls, stats_type, data_set):
+    def __init__(self, dataset):
+        self._logger = LoggingHelper.get_logger(self.__class__.__name__)
+        self._logger.info('initializing stats helper')
+        self._dataset = dataset
+        self._supported_stats = {
+            self.OVERALL_SUMMARY: self.create_overall_summary
+        }
+
+    def create_stats(self, stats_type):
         """Identify the type of stats and calculate
         them by calling the appropriate method.
         """
+        self._logger.info('requested stats: %s', stats_type)
+        try:
+            self._supported_stats[stats_type]()
+        except KeyError:
+            raise ValueError(f'unsupported stats type: {stats_type}')
 
     # TODO: think how to decouple transaction structure
     # maybe init, add, finish methods and pass values as args
-    @classmethod
-    def create_overall_summary(cls, dataset):
+    def create_overall_summary(self):
         """Calculate an overall summary from data_set.
 
         The returned object looks like the following structure:
@@ -33,8 +47,9 @@ class StatsHelper:
             }
         }
         """
+        self._logger.info('creating overall summary')
         result = {}
-        for data in dataset:
+        for data in self._dataset:
             year = datetime.datetime.strptime(data.date, '%Y-%m-%d').year
             month = datetime.datetime.strptime(data.date, '%Y-%m-%d').month
             # add year if it doesn't exists
@@ -44,19 +59,19 @@ class StatsHelper:
             # add month if it doesn't exists for year
             if month not in result[year]:
                 result[year][month] = {
-                    cls.TRANSACTIONS: [],
-                    cls.TOTAL_PER_TAG: {},
+                    self.TRANSACTIONS: [],
+                    self.TOTAL_PER_TAG: {},
                 }
 
             # append transaction in data[year][month][transactions]
-            result[year][month][cls.TRANSACTIONS].append(data)
+            result[year][month][self.TRANSACTIONS].append(data)
 
             # insert tag and current value in data[year][month][total_per_tag]
             # or add current value to existing match
             for tag in data.tags:
-                if tag in result[year][month][cls.TOTAL_PER_TAG]:
-                    result[year][month][cls.TOTAL_PER_TAG][tag] += data.amount
+                if tag in result[year][month][self.TOTAL_PER_TAG]:
+                    result[year][month][self.TOTAL_PER_TAG][tag] += data.amount
                 else:
-                    result[year][month][cls.TOTAL_PER_TAG][tag] = data.amount
+                    result[year][month][self.TOTAL_PER_TAG][tag] = data.amount
 
             return result
