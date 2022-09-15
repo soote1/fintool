82dc83220e2752acb789cff5da36317be84f718d
Isaias Ramirez <soote1.ir@gmail.com>
2022-09-14 22:20:46 -0500

    Add multiline chart
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

--- fintool/charts.py
+++ fintool/charts.py
@@ -2,6 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+class MultiLineChart:
+    def draw(self, title, ylabel, labels, line_values):
+        x = np.arange(len(labels))  # the label locations
+        fig, ax = plt.subplots()
+
+        # Build lines
+        for label, y_values in line_values.items():
+            ax.plot(x, y_values, label=label)
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel(ylabel)
+        ax.set_title(title)
+        ax.set_xticks(x, labels)
+        ax.legend()
+
+        fig.tight_layout()
+
+        plt.show()
+
 class BarChart:
     def draw(self, title, ylabel, labels, bar_values):
         """"""
