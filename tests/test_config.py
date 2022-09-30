3a27acd5dfdc90a6be89a03be50e64d4cb9c5a0b
Isaias Ramirez <soote1.ir@gmail.com>
2022-09-29 22:58:12 -0500

    Implement unit tests for ConfigManager
    Signed-off-by: Isaias Ramirez <soote1.ir@gmail.com>

---
+++ tests/test_config.py
@@ -0,0 +1,34 @@
+import unittest
+
+from fintool.config import ConfigManager
+
+
+class TestConfig(unittest.TestCase):
+    """
+    ConfigManager test suite.
+    """
+    def test_get_value(self):
+        """
+        Make sure that the config manager returns the expected value.
+        """
+        expected = 3
+        initial_cfg = {'a': 1, 'b': 2, 'c': {'d': {'e': 3}}}
+
+        ConfigManager.init(initial_cfg)
+        actual = ConfigManager.get('c.d.e')
+
+        self.assertEqual(actual, expected)
+
+    def test_set_value(self):
+        """
+        Make sure that the config manager sets the value in the corresponding
+        key and in the expected level of the parent object.
+        """
+        expected = {'a': 1, 'b': 2, 'c': {'d': {'e': 3}}}
+        initial_cfg = {'a': 1, 'b': 2}
+
+        ConfigManager.init(initial_cfg)
+        ConfigManager.set('c.d.e', 3)
+        actual = ConfigManager.get_cfg()
+
+        self.assertEqual(actual, expected)
